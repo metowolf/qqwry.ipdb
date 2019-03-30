be4a49727cd6152b7eddfe4c036a32a52a7b543a
metowolf <i@i-meto.com>
2019-03-30 23:05:38 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ node src/packer.js
 node src/packer_raw.js
 
 LATEST_VERSION=`node src/version.js`
-CURRENT_VERSION=`curl https://cdn.jsdelivr.net/npm/qqwry.ipdb/package.json?r=$LATEST_VERSION |grep -m1 version | awk -F: '{ print $2 }' | sed 's/[", ]//g'`
+CURRENT_VERSION=`curl http://registry.npmjs.org/qqwry.ipdb | jq -r '."dist-tags".latest'`
 
 if [ $CURRENT_VERSION != $LATEST_VERSION ]; then
   cd build/raw
