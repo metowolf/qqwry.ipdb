1403e1e4e51e488f0ab36a8748c00984efbcdd4d
METO <i@i-meto.com>
2019-11-16 11:04:19 +0800

    fix(qqwry): skip waf

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -e
 mkdir -p build/stand
 mkdir -p build/raw
 
-wget http://update.cz88.net/ip/copywrite.rar -4 -O build/copywrite.rar
-wget http://update.cz88.net/ip/qqwry.rar -4 -O build/qqwry.rar
+wget http://update.cz88.net/ip/copywrite.rar -4 -O build/copywrite.rar --user-agent="Mozilla/3.0 (compatible; Indy Library)"
+wget http://update.cz88.net/ip/qqwry.rar -4 -O build/qqwry.rar --user-agent="Mozilla/3.0 (compatible; Indy Library)"
 
 node src/decode.js
 node src/packer.js
