85de669eeec14e0f74a65c6dc59e7f3cd25bf909
Peyton Yip <ye1324257081@gmail.com>
2021-03-06 11:20:47 +0800

    remove upload to npm

--- build.sh
+++ build.sh
@@ -12,17 +12,3 @@ node src/decode.js
 node src/packer.js
 node src/packer_raw.js
 
-LATEST_VERSION=`node src/version.js`
-CURRENT_VERSION=`curl http://registry.npmjs.org/qqwry.ipdb | jq -r '."dist-tags".latest'`
-
-if [ $CURRENT_VERSION != $LATEST_VERSION ]; then
-  cp README.md build/raw/README.md
-  cd build/raw
-  npm publish
-  cd ../..
-
-  cp README.md build/stand/README.md
-  cd build/stand
-  npm publish
-  cd ../..
-fi
