c39c09489a22484f7ee89d986b8839ac1156222a
Peyton Yip <ye1324257081@gmail.com>
2021-04-07 21:37:38 +0800

    fix some errors

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 set -e
 
 chunzhen_url=http://update.cz88.net/soft/setup.zip
-temp_dir=/tmp/qqwry
-current_dir=$(pwd)
-innoextract_path=`pwd`/exe_tool/innoextract
+#temp_dir=/tmp/qqwry
+#current_dir=$(pwd)
+innoextract_path=$(pwd)/exe_tool/innoextract
 
 # prepare
-rm -rfv $temp_dir
-mkdir $temp_dir
-cd $temp_dir
+#rm -rfv $temp_dir
+#mkdir $temp_dir
+#cd $temp_dir
 #apt install innoextract -y
 
 # down
@@ -18,14 +18,16 @@ wget $chunzhen_url
 
 # unzip
 unzip setup.zip
+chmod +x "$innoextract_path"
+ls -al "$innoextract_path"
 $innoextract_path setup.exe
 # copy
 #qqwry_file=app/qqwry.dat
 
-cd "$current_dir" || exit
+#cd "$current_dir" || exit
 mkdir -p build/stand
 mkdir -p build/raw
-cp "$temp_dir"/app/qqwry.dat ./build/stand/
+cp ./app/qqwry.dat ./build/stand/
 
 
 
